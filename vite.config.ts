1dcc0a10623727e8be0e445fc08955927704f812
AJoelCastro <misthorn33@gmail.com>
2026-02-11 16:34:34 -0500

    feat: vite library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url)) 
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +12,21 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'e-tricks-library',
+      fileName: 'e-tricks-library',
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: 'React',
+          "react-dom": "ReactDom",
+          "react/jsx-runtime": "react/jsx-runtime"
+        },
+      },
+    },
+  },
 })
